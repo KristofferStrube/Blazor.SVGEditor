74c421f8d83f0a0efd8bf8e505135a9362305550
Kristoffer Strube <kristoffer.strube@gmail.com>
2021-06-20 21:10:15 +0200

    Got helper-ellipse to work

--- src/KristofferStrube.Blazor.SVGEditor/PathData/EllipticalArcInstruction.cs
+++ src/KristofferStrube.Blazor.SVGEditor/PathData/EllipticalArcInstruction.cs
@@ -39,73 +39,63 @@ namespace KristofferStrube.Blazor.SVGEditor
 
         public (double x, double y) ControlPoint
         {
-            get {
-                var theta = xAxisRotation;
-                if ((x + StartPosition.x) < 0)
+            get
+            {
+                return (Center.x + EllipseRadi.Ry * Math.Sin(xAxisRotation / 180 * Math.PI), Center.y - EllipseRadi.Ry * Math.Cos(xAxisRotation / 180 * Math.PI));
+            }
+        }
+
+        private double x1Mark => (x - StartPosition.x) / 2 * Math.Cos(xAxisRotation / 180.0 * Math.PI) + (y - StartPosition.y) / 2 * Math.Sin(xAxisRotation / 180.0 * Math.PI);
+        private double y1Mark => (x - StartPosition.x) / 2 * -Math.Sin(xAxisRotation / 180.0 * Math.PI) + (y - StartPosition.y) / 2 * Math.Cos(xAxisRotation / 180.0 * Math.PI);
+
+        // These use the implementation from https://svgwg.org/svg2-draft/implnote.html#ArcConversionEndpointToCenter
+        public (double x, double y) Center
+        {
+            get
+            {
+                var sign = (largeArcFlag == sweepFlag ? 1 : -1);
+
+                var rxPow = Math.Pow(EllipseRadi.Rx, 2);
+                var ryPow = Math.Pow(EllipseRadi.Ry, 2);
+                var x1MarkPow = Math.Pow(x1Mark, 2);
+                var y1MarkPow = Math.Pow(y1Mark, 2);
+
+                double sqrtPart;
+                if (Delta <= 1)
                 {
-                    theta = -theta;
+                    sqrtPart = sign * Math.Sqrt((rxPow * ryPow - rxPow * y1MarkPow - ryPow * x1MarkPow) / (rxPow * y1MarkPow + ryPow * x1MarkPow));
                 }
-                return (
-                    (x + StartPosition.x) / 2 +
-                    Math.Cos(theta / 180 * Math.PI) *
-                    (rx / ry) *
-                    Math.Sqrt(Math.Pow((y - StartPosition.y), 2) +
-                    Math.Pow((x - StartPosition.x), 2)) / 2,
-
-                    (y + StartPosition.y) / 2 +
-                    Math.Sin(theta / 180 * Math.PI) *
-                    (rx / ry) *
-                    Math.Sqrt(Math.Pow((y - StartPosition.y), 2) +
-                    Math.Pow((x - StartPosition.x), 2)) / 2
-                );
-            }
-        }
-
-        public double cx
-        {
-            get
-            {
-                return (x + StartPosition.x) / 2.0;
-            }
-        }
-
-        public double cy
-        {
-            get
-            {
-                return (y + StartPosition.y) / 2.0;
-            }
-        }
-
-        public double ellipseRx
-        {
-            get
-            {
-                var definedDistance = Math.Sqrt(Math.Pow(rx, 2) + Math.Pow(ry, 2));
-                var actualDistance = Math.Sqrt(Math.Pow(x - StartPosition.x, 2) + Math.Pow(y - StartPosition.y, 2));
-                if (actualDistance > definedDistance)
+                else
                 {
-                    return rx * (actualDistance / definedDistance);
+                    sqrtPart = 0;
                 }
-                return rx;
+
+                var cxMark = sqrtPart * ((EllipseRadi.Rx * y1Mark) / EllipseRadi.Ry);
+                var cyMark = sqrtPart * (-(EllipseRadi.Ry * x1Mark) / EllipseRadi.Rx);
+
+                var cx = Math.Cos(xAxisRotation / 180.0 * Math.PI) * cxMark - Math.Sin(xAxisRotation / 180.0 * Math.PI) * cyMark + (StartPosition.x + x) / 2;
+                var cy = Math.Sin(xAxisRotation / 180.0 * Math.PI) * cxMark + Math.Cos(xAxisRotation / 180.0 * Math.PI) * cyMark + (StartPosition.y + y) / 2;
+
+                return (cx, cy);
             }
         }
 
-        public double ellipseRy
+        private double Delta => Math.Pow(x1Mark, 2) / Math.Pow(Math.Abs(rx), 2) + Math.Pow(y1Mark, 2) / Math.Pow(Math.Abs(ry), 2);
+
+        public (double Rx, double Ry) EllipseRadi
         {
             get
             {
-                var definedDistance = Math.Sqrt(Math.Pow(rx, 2) + Math.Pow(ry, 2));
-                var actualDistance = Math.Sqrt(Math.Pow(x - StartPosition.x, 2) + Math.Pow(y - StartPosition.y, 2));
-                if (actualDistance > definedDistance)
+                if (Delta <= 1)
                 {
-                    return ry * (actualDistance / definedDistance);
+                    return (Math.Abs(rx), Math.Abs(ry));
                 }
-                return ry;
+                return (Math.Sqrt(Delta) * Math.Abs(rx), Math.Sqrt(Delta) * Math.Abs(ry));
             }
         }
 
-        public override (double x, double y) EndPosition {
+        public override (double x, double y) EndPosition
+        {
             get => (x, y);
             set { x = value.x; y = value.y; }
         }
