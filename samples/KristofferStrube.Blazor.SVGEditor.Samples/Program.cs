b16d1a2768b5d13f4ddc8acc9a495c5ad553f084
Kristoffer Strube <kristoffer.strube@gmail.com>
2021-07-16 12:27:20 +0200

    Moved Zoom function to be in ContextMenu

--- samples/KristofferStrube.Blazor.SVGEditor.Samples/Program.cs
+++ samples/KristofferStrube.Blazor.SVGEditor.Samples/Program.cs
@@ -19,6 +19,8 @@ namespace KristofferStrube.Blazor.SVGEditor.Samples
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddSVGEditor();
+
             await builder.Build().RunAsync();
         }
     }
---
+++ src/KristofferStrube.Blazor.SVGEditor/ServiceExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KristofferStrube.Blazor.SVGEditor
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddSVGEditor(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection.AddBlazorContextMenu();
+        }
+    }
+}
