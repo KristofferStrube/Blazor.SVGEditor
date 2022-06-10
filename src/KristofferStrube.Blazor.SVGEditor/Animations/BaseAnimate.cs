d40dae29370f6b2fd1d7953982ee57138c294dbf
Kristoffer Strube <kristoffer.strube@gmail.com>
2022-06-10 02:57:40 +0200

    Shift start time

--- src/KristofferStrube.Blazor.SVGEditor/Animations/BaseAnimate.cs
+++ src/KristofferStrube.Blazor.SVGEditor/Animations/BaseAnimate.cs
@@ -23,6 +23,7 @@ public abstract class BaseAnimate : ISVGElement
     public bool Playing { get; set; }
     public List<string> Values { get; set; }
     public int FrameCount => Values.Count;
+    public double Begin => Element.GetAttribute("begin") is string s ? s.Replace("s", "").ParseAsDouble() : 0;
     public double Dur => Element.GetAttribute("dur") is string s ? s.Replace("s", "").ParseAsDouble() : 0;
     public string AttributeName => Element.GetAttributeOrEmpty("attributename");
     public string ValuesAsString => Element.GetAttributeOrEmpty("values");
