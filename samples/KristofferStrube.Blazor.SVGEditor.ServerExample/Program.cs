5fd4fb21300aab5aa309d405fb1bfef0cc86fa6d
KristofferStrube <kristoffer.strube@gmail.com>
2023-07-16 13:17:46 +0200

    Sample pages restructure and added minimal Blazor Server example.

---
+++ samples/KristofferStrube.Blazor.SVGEditor.ServerExample/Program.cs
@@ -0,0 +1,30 @@
+
+using KristofferStrube.Blazor.SVGEditor.Extensions;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddSVGEditor();
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    _ = app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    _ = app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
