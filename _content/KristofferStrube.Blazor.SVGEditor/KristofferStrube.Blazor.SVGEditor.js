fdae2d5cc80f5141b70f49bf1a64fc9e2cb3ee7c
KristofferStrube <KristofferStrube@users.noreply.github.com>
2025-09-10 11:17:01 +0000

    deploy: 4b050186740b5af03b72bea2c0c1b3e7f31cbce0

---
+++ _content/KristofferStrube.Blazor.SVGEditor/KristofferStrube.Blazor.SVGEditor.js
@@ -0,0 +1,5 @@
+﻿export function focus(element) { if (element) { element.focus() } }
+
+export function BBox(element) { return element ? element.getBoundingClientRect() : {}; }
+
+export function unFocus(element) { if (element) { element.blur() } }
